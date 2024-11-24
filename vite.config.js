b90323f6ce807efe0ccd350d1bac88d6a335e042
rajat12826 <pariharrajat078@gmail.com>
2024-11-24 19:17:18 +0530

    m

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
+  },
+  publicDir: 'public',
   plugins: [react()],
   resolve: {
     alias: {
